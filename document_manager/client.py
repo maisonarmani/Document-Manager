57ce87f333d7981536545338e3c8f375841bd64e
masonarmani38@gmail.com <masonarmani38@gmail.com>
2019-03-03 09:21:15 +0000

    update

--- document_manager/client.py
+++ document_manager/client.py
@@ -5,7 +5,7 @@
 from __future__ import unicode_literals
 
 import frappe
-from frappe import share,_
+from frappe import share, _
 
 
 def update_customer_folder_structure(customer):
@@ -15,9 +15,12 @@ def update_customer_folder_structure(customer):
 
     root = create_client_root_folder()
 
+    share_file_with_customer_user_str(root, customer_email, notify=0)
+    share_file_with_customer_user_str("Home", customer_email, notify=0)
+
     folders, client_name = [], customer.name
     client_structure = get_structure(structure="Primary", client=client_name)
-    folders.append({"parent":root, "folder_name": client_name})
+    folders.append({"parent": root, "folder_name": client_name})
     for (key, client) in client_structure.items():
         if isinstance(client, dict):
             for (k, v) in client.items():
@@ -28,7 +31,6 @@ def update_customer_folder_structure(customer):
                     for i in v:
                         folders.append({"parent": "{0}/{1}".format(parent, k), "folder_name": i})
 
-
     frappe.errprint("folders %s" % folders)
     for folder in folders:
         create_new_folder(folder.get('folder_name'), folder.get('parent'), customer_email)
@@ -38,7 +40,8 @@ def get_structure(structure=None, client=None):
     def get_children(structure, parent):
         # check if there's a default set
         check = frappe.db.sql(
-            "select fsi.child from `tabFolder Structure` fs inner join `tabFolder Structure Item` fsi limit 1", as_list=1)
+            "select fsi.child from `tabFolder Structure` fs inner join `tabFolder Structure Item` fsi limit 1",
+            as_list=1)
 
         if check == []:
             frappe.throw("Sorry, No folder structure found")
@@ -66,11 +69,11 @@ def get_structure(structure=None, client=None):
 
     return {client: ls}
 
+
 def create_client_root_folder():
     # get home
-    name = "{0}/{1}".format("Home","Clients")
+    name = "{0}/{1}".format("Home", "Clients")
     frappe.errprint("creating root %s" % name)
-    frappe.errprint(frappe.db.sql("select name from tabFile where name = '%s'" % name))
     if not frappe.db.exists("File", name):
         file = frappe.get_doc({
             "doctype": "File",
@@ -83,10 +86,11 @@ def create_client_root_folder():
         frappe.db.commit()
         return file.name
 
-    return  name
+    return name
+
 
 def create_new_folder(file_name, folder, user):
-    name = "{0}/{1}".format(folder,file_name)
+    name = "{0}/{1}".format(folder, file_name)
     frappe.errprint("creating %s" % name)
     frappe.errprint(frappe.db.sql("select name from tabFile where name = '%s'" % name))
 
@@ -104,11 +108,19 @@ def create_new_folder(file_name, folder, user):
         # share file with customer user
         share_file_with_customer_user(file, user)
 
+    add_user_icon("File", user, label=file_name, link="List/File/%s"%name, standard=0)
+
 
 def share_file_with_customer_user(file, user, notify=0):
+    name = file.get("name")
+    frappe.errprint("sharing %s with %s" % (name, user))
+    share.add("File", name, user=user, read=1, write=1, share=0, everyone=0,
+              flags=None, notify=notify)
 
-    frappe.errprint("sharing %s with %s" % (file.name, user))
-    share.add(file.doctype, file.name, user=user, read=1, write=1, share=0, everyone=0,
+
+def share_file_with_customer_user_str(file, user, notify=0):
+    frappe.errprint("sharing %s with %s" % (file, user))
+    share.add("File", file, user=user, read=1, write=1, share=0, everyone=0,
               flags=None, notify=notify)
 
 
@@ -124,11 +136,17 @@ def create_customer_user(customer):
         "doctype": "User",
         "user_type": "System User",
         "email": email,
-        "send_welcome_email": 1,
-        "first_name": fullname or email.split("@")[0]
+        "send_welcome_email": 0,
+        "first_name": fullname or email.split("@")[0],
+        "block_modules": [{"module": "Buying"}]
     })
     user.add_roles('File User')
+    user.insert(ignore_permissions=True)
     user.save(ignore_permissions=True)
+
+
+    # Block other module view
+
 
 @frappe.whitelist()
 def update_all(customer, trigger=""):
@@ -139,12 +157,11 @@ def update_all(customer, trigger=""):
         frappe.errprint(err)
 
 
-
 @frappe.whitelist()
 def append_permission(doc, trigger=""):
     frappe.errprint("doc  %s" % doc.__dict__)
     users = []
-    docshares = share.get_users("File",doc.folder)
+    docshares = share.get_users("File", doc.folder)
     frappe.errprint("docshares  %s" % docshares)
 
     for docshare in docshares:
@@ -153,4 +170,102 @@ def append_permission(doc, trigger=""):
             users.index(docshare.user)
         except ValueError:
             users.append(docshare.user)
-            share_file_with_customer_user(doc, docshare.user, 1)
+            share_file_with_customer_user(doc, docshare.user, 0)
+
+
+def add_user_icon(_doctype, user, _report=None, label=None, link=None, type='link', standard=0):
+    '''Add a new user desktop icon to the desktop'''
+
+    if not label: label = _doctype or _report
+    if not link: link = 'List/{0}'.format(_doctype)
+
+    frappe.errprint("attempt to add desktop with  %s" % user)
+
+    # find if a standard icon exists
+    icon_name = frappe.db.exists('Desktop Icon', {'standard': standard, 'link': link,
+                                                  'owner': user})
+
+    if icon_name:
+        if frappe.db.get_value('Desktop Icon', icon_name, 'hidden'):
+            # if it is hidden, unhide it
+            frappe.db.set_value('Desktop Icon', icon_name, 'hidden', 0)
+            clear_desktop_icons_cache()
+
+    else:
+        idx = frappe.db.sql('select max(idx) from `tabDesktop Icon` where owner=%s', user)[0][0] or \
+              frappe.db.sql('select count(*) from `tabDesktop Icon` where standard=1')[0][0]
+
+        if not frappe.db.get_value("Report", _report):
+            _report = None
+            userdefined_icon = frappe.db.get_value('DocType', _doctype, ['icon', 'module'], as_dict=True)
+        else:
+            userdefined_icon = frappe.db.get_value('Report', _report, ['icon', 'module'], as_dict=True)
+
+        module_icon = frappe.get_value('Desktop Icon', {'standard': 1, 'module_name': userdefined_icon.module},
+                                       ['name', 'icon', 'color', 'reverse'], as_dict=True)
+
+        if not module_icon:
+            module_icon = frappe._dict()
+            opts = random.choice(32)
+            module_icon.color = opts[0]
+            module_icon.reverse = 0 if (len(opts) > 1) else 1
+
+        try:
+            new_icon = frappe.get_doc({
+                'doctype': 'Desktop Icon',
+                'label': label,
+                'module_name': label,
+                'link': link,
+                'type': type,
+                '_doctype': _doctype,
+                '_report': _report,
+                'icon': "octicon-file-directory",
+                'color': module_icon.color,
+                'reverse': module_icon.reverse,
+                'idx': idx + 1,
+                'custom': 1,
+                'hidden': 0,
+                'standard': standard
+            })
+            new_icon.owner = user
+            new_icon.insert(ignore_permissions=True)
+            clear_desktop_icons_cache(user)
+
+            icon_name = new_icon.name
+
+        except frappe.UniqueValidationError as e:
+            frappe.errprint(_('Desktop Icon already exists %s' % e))
+
+    return icon_name
+
+
+def get_all_icons():
+    return [d.module_name for d in frappe.get_all('Desktop Icon',
+                                                  filters={'standard': 1}, fields=['module_name'])]
+
+
+def clear_desktop_icons_cache(user=None):
+    frappe.cache().hdel('desktop_icons', user or frappe.session.user)
+    frappe.cache().hdel('bootinfo', user or frappe.session.user)
+
+
+def get_modules():
+    modules = [m.module_name for m in frappe.db.get_all('Desktop Icon',
+                  fields=['module_name'], filters={'standard': 1}, order_by="module_name")]
+
+    frappe.errprint("Modules %s", modules)
+    return modules
+
+def block(user, module):
+    block_module = frappe.db.new_doc({
+        "doctype":"Block Module",
+        "module":module,
+        "owner":user
+    })
+    frappe.errprint("Blocked modules %s", block_module)
+    block_module.insert()
+
+
+def block_modules_for_user(user):
+    for module in get_modules():
+        block(module, user)
